6511c4dd48a85dce411170dfce8262e94f1e2263
Catheryne Small <catheryn.small@gmail.com>
2020-05-29 01:04:24 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@artists.push(artist)
+    @@genres.push(genre)
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    @@genres.uniq.map { |genre| [genre, @@genres.count(genre)] }.to_h
+  end
+  
+  def self.artist_count
+    @@artists.uniq.map { |artist| [artist, @@artists.count(artist)] }.to_h
+  end
+end
