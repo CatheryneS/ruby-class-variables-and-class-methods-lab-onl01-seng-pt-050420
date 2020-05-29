f4ca9d8e27d753bd970b3a82af51070fec5ab0e0
Catheryne Small <catheryn.small@gmail.com>
2020-05-29 00:38:20 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@artists.push(artist)
+    @@genres.push(genre)
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.each_with_index do |genre, count|
+      
+  end
+end
